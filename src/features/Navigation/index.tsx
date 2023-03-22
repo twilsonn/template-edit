7ced0e93224c54b5dde2db14f23cb784961d402f
Tyler Wilson <tylerwilson0729@outlook.com>
2023-03-22 21:00:41 +0000

    update: added Sidebar

--- src/features/Navigation/index.tsx
+++ src/features/Navigation/index.tsx
@@ -1,20 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import { Logo } from "../../assets/icons/Logo";
 import { Menu } from "../../assets/icons/Menu";
+import Sidebar from "./components/Sidebar";
 
 const Navigation = () => {
-  return (
-    <nav className="h-12 w-full bg-neutral-800 flex items-center space-x-6">
-      <button className="select-none h-full bg-neutral-700 hover:bg-neutral-600 px-6">
-        <Menu className="w-6 h-6 text-neutral-400" />
-      </button>
+  const [open, setOpen] = useState(false);
 
-      <button className="flex items-center space-x-3 select-none h-full">
-        <Logo className="w-8 h-8 text-blue-400" />
-        <h1 className="text-xl text-neutral-50 tracking-wider font-semibold">
-          TemplateEdit
-        </h1>
-      </button>
+  const handleMenuOnClick = () => {
+    setOpen(!open);
+  };
+
+  return (
+    <nav className="relative h-12 w-full bg-neutral-800">
+      <div className="flex items-center h-full space-x-6">
+        <button
+          className="select-none h-full bg-neutral-700 hover:bg-neutral-600 px-6"
+          onClick={handleMenuOnClick}
+        >
+          <Menu className="w-6 h-6 text-neutral-400" />
+        </button>
+
+        <button className="flex items-center space-x-3 select-none h-full">
+          <Logo className="w-7 h-7 text-blue-400" />
+          <h1 className="text-xl text-neutral-300 font-semibold font-brand">
+            TemplateEdit
+          </h1>
+        </button>
+      </div>
+
+      <Sidebar open={open} setOpen={setOpen} />
     </nav>
   );
 };
