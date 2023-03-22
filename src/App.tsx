038cfe890dafc7bafe08613941c9fc44b1b384b1
Tyler Wilson <tylerwilson0729@outlook.com>
2023-03-22 18:06:16 +0000

    update: exracted code to features

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,11 @@
-import { useWindowSize } from "usehooks-ts";
-import SplitPane from "./components/SplitPane";
-import { Editor } from "./features/Editor";
+import EditorWindow from "./features/EditorWindow";
+import Navigation from "./features/Navigation";
 
 function App() {
-  const { width, height } = useWindowSize();
-
-  const sizes = {
-    minSize: 200,
-    maxWidth: width - 200,
-    maxHeight: height - 200,
-    defaultWidth: width / 2,
-    defaultHeight: height - height / 3,
-  };
-
   return (
-    <div className="h-screen w-screen">
-      <SplitPane
-        className="bg-neutral-100"
-        split="vertical"
-        size={sizes.defaultWidth}
-        minSize={sizes.minSize}
-        maxSize={sizes.maxWidth}
-      >
-        <SplitPane
-          className="bg-neutral-900"
-          split="horizontal"
-          size={sizes.defaultHeight}
-          minSize={sizes.minSize}
-          maxSize={sizes.maxHeight}
-        >
-          <Editor initialContent="test" language="twig" />
-          <Editor initialContent="test" language="json" />
-        </SplitPane>
-        <div />
-      </SplitPane>
+    <div className="h-screen w-screen flex flex-col">
+      <Navigation />
+      <EditorWindow />
     </div>
   );
 }
