9c2cc90813edcf6872618df013dc90f2d716515b
Tyler Wilson <tylerwilson0729@outlook.com>
2023-03-22 20:59:09 +0000

    update: added name and lastUpdated props

--- src/features/EditorWindow/TemplateEditor.tsx
+++ src/features/EditorWindow/TemplateEditor.tsx
@@ -2,6 +2,7 @@ import { useMap } from "usehooks-ts";
 import { templatesAtom } from "./state";
 import { useAtom } from "jotai";
 import Editor from "./components/Editor";
+import { useState } from "react";
 
 interface ITemplateEditor {
   height: number;
@@ -10,18 +11,21 @@ interface ITemplateEditor {
 
 const TemplateEditor: React.FC<ITemplateEditor> = ({ height, width }) => {
   const [templates, setTemplates] = useAtom(templatesAtom);
-  const [map, actions] = useMap<string, string>(templates);
+  const [map, actions] = useMap(templates);
+  const [activeTemplate, setActiveTemplate] = useState(map.get("default"));
 
   const handleEditorOnChange: (content: string) => void = (content) => {
-    actions.set("default", content);
-    setTemplates(Array.from(map.entries()));
+    if (activeTemplate) {
+      actions.set("default", { ...activeTemplate, content });
+      setTemplates(Array.from(map.entries()));
+    }
   };
 
   return (
     <Editor
       height={height}
       width={width}
-      initialContent={map.get("default") || ""}
+      initialContent={map.get("default")?.content || ""}
       language="twig"
       onChange={handleEditorOnChange}
     />
--- src/features/EditorWindow/state/templateState.ts
+++ src/features/EditorWindow/state/templateState.ts
@@ -1,12 +1,24 @@
 import { MapOrEntries } from "usehooks-ts";
 import { atomWithLocalStorage } from "../../../utils/AtomWithLocalStorage";
 
-const templatesAtom = atomWithLocalStorage<MapOrEntries<string, string>>(
+interface ITemplate {
+  content: string;
+  data?: object;
+  name: string;
+  lastUpdated: number;
+}
+
+const templatesAtom = atomWithLocalStorage<MapOrEntries<string, ITemplate>>(
   "templates",
   [
     [
       "default",
-      '<!DOCTYPE html>\r\n<html>\r\n\t<head>\r\n\t<title>My Webpage</title>\r\n\t</head>\r\n\t<body>\r\n\t<ul id="navigation">\r\n\t{% for item in navigation %}\r\n\t<li><a href="{{ item.href }}">{{ item.caption }}</a></li>\r\n\t{% endfor %}\r\n\t</ul>\r\n\r\n\t<h1>My Webpage</h1>\r\n\t{{ a_variable }}\r\n\t</body>\r\n</html>\r\n',
+      {
+        content:
+          '<!DOCTYPE html>\r\n<html>\r\n\t<head>\r\n\t<title>My Webpage</title>\r\n\t</head>\r\n\t<body>\r\n\t<ul id="navigation">\r\n\t{% for item in navigation %}\r\n\t<li><a href="{{ item.href }}">{{ item.caption }}</a></li>\r\n\t{% endfor %}\r\n\t</ul>\r\n\r\n\t<h1>My Webpage</h1>\r\n\t{{ a_variable }}\r\n\t</body>\r\n</html>\r\n',
+        name: "Default Template",
+        lastUpdated: new Date(Date.now()).getTime(),
+      },
     ],
   ]
 );
