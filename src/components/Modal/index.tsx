c58c58f40050672005c8bb3ae0d178ef2621303f
Tyler Wilson <tylerwilson0729@outlook.com>
2023-03-22 22:32:18 +0000

    add: added Modal

---
+++ src/components/Modal/index.tsx
@@ -0,0 +1,29 @@
+import React, { PropsWithChildren, useRef } from "react";
+import { Portal } from "react-portal";
+import { useOnClickOutside } from "usehooks-ts";
+
+import "./index.css";
+
+interface IModal {
+  open: boolean;
+  setOpen: (open: boolean) => void;
+}
+
+const Modal: React.FC<PropsWithChildren<IModal>> = ({
+  children,
+  open,
+  setOpen,
+}) => {
+  const ModalRef = useRef(null);
+  useOnClickOutside(ModalRef, () => setOpen(false));
+
+  return (
+    <Portal>
+      <div className={`modal-wrapper ${open ? "open" : ""}`}>
+        <div className="modal-container">{children}</div>
+      </div>
+    </Portal>
+  );
+};
+
+export default Modal;
