4b3fb9293f9a561b959fceeae03896b8d07b6056
Tyler Wilson <tylerwilson0729@outlook.com>
2023-03-22 22:36:03 +0000

    update: added ID to template object

--- src/features/EditorWindow/state/templateState.ts
+++ src/features/EditorWindow/state/templateState.ts
@@ -1,26 +1,37 @@
 import { MapOrEntries } from "usehooks-ts";
 import { atomWithLocalStorage } from "../../../utils/AtomWithLocalStorage";
+import { uniqueId } from "../../../utils/generateId";
 
-interface ITemplate {
-  content: string;
-  data?: object;
-  name: string;
-  lastUpdated: number;
+interface ITemplates {
+  activeTemplate: string;
+  templates: MapOrEntries<
+    string,
+    {
+      id: string;
+      content: string;
+      data?: object;
+      name: string;
+      lastUpdated: number;
+    }
+  >;
 }
 
-const templatesAtom = atomWithLocalStorage<MapOrEntries<string, ITemplate>>(
-  "templates",
-  [
+const defaultId = uniqueId();
+
+const templatesAtom = atomWithLocalStorage<ITemplates>("templates", {
+  activeTemplate: defaultId,
+  templates: [
     [
-      "default",
+      defaultId,
       {
+        id: defaultId,
         content:
           '<!DOCTYPE html>\r\n<html>\r\n\t<head>\r\n\t<title>My Webpage</title>\r\n\t</head>\r\n\t<body>\r\n\t<ul id="navigation">\r\n\t{% for item in navigation %}\r\n\t<li><a href="{{ item.href }}">{{ item.caption }}</a></li>\r\n\t{% endfor %}\r\n\t</ul>\r\n\r\n\t<h1>My Webpage</h1>\r\n\t{{ a_variable }}\r\n\t</body>\r\n</html>\r\n',
         name: "Default Template",
         lastUpdated: new Date(Date.now()).getTime(),
       },
     ],
-  ]
-);
+  ],
+});
 
 export { templatesAtom };
