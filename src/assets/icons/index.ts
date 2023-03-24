3447d83fdfe3ccc36912a537e79c254c8af0c69b
Tyler Wilson <tylerwilson0729@outlook.com>
2023-03-24 23:41:47 +0000

    update: added clean hand icon

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -4,3 +4,4 @@ export { Link } from "./Link";
 export { ChevronRight } from "./ChevronRight";
 export { PenToSquare } from "./PenToSquare";
 export { Plus } from "./Plus";
+export { CleanHand } from "./CleanHand";
