7b132869ff2d41d020f134d4c20f5648de1a7fb0
Tyler Wilson <tylerwilson0729@outlook.com>
2023-03-24 23:43:53 +0000

    feat: added abilty to format code

--- src/features/EditorWindow/TemplateEditor.tsx
+++ src/features/EditorWindow/TemplateEditor.tsx
@@ -1,37 +1,91 @@
-import { useMap } from "usehooks-ts";
-import { templatesAtom } from "./state";
+import { useEffect, useRef } from "react";
 import { useAtom } from "jotai";
 import Editor from "./components/Editor";
-import { useState } from "react";
+import Button from "@/components/Button";
+import { templatesAtom } from "./state";
+import { CleanHand } from "@/assets/icons";
 
 interface ITemplateEditor {
   height: number;
   width: number;
 }
 
-const TemplateEditor: React.FC<ITemplateEditor> = ({ height, width }) => {
-  const [{ activeTemplate, templates }, setTemplates] = useAtom(templatesAtom);
-  const [map, actions] = useMap(templates);
+const formatContent = (content: string) =>
+  fetch("/api/format", {
+    method: "POST", // *GET, POST, PUT, DELETE, etc.
+    mode: "cors",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      type: "twig",
+      content,
+    }),
+  })
+    .then((req) => req.json())
+    .then((data) => {
+      return data.content;
+    });
 
-  const handleEditorOnChange: (content: string) => void = (content) => {
-    const currentTemplate = map.get(activeTemplate);
+const TemplateEditor: React.FC<ITemplateEditor> = ({ height, width }) => {
+  const editorRef = useRef(null);
+  const [{ activeTemplate, templates }, setTemplates] = useAtom(templatesAtom);
+
+  useEffect(() => {
+    formatTemplate();
+  }, [activeTemplate]);
+
+  const updateTemplate = (newContent: string) => {
+    const currentTemplate = templates[activeTemplate];
+
     if (currentTemplate) {
-      actions.set(activeTemplate, { ...currentTemplate, content });
+      const newVersion = {
+        [activeTemplate]: {
+          name: currentTemplate.name,
+          lastUpdated: new Date(Date.now()).getTime(),
+          content: newContent,
+        },
+      };
+
       setTemplates({
         activeTemplate,
-        templates: Array.from(map.entries()),
+        templates: {
+          ...templates,
+          ...newVersion,
+        },
       });
     }
   };
 
+  const formatTemplate = () => {
+    const currentTemplate = templates[activeTemplate];
+    formatContent(currentTemplate.content).then((formattedContent) => {
+      updateTemplate(formattedContent);
+      // @ts-ignore
+      editorRef.current?.setValue(formattedContent);
+    });
+  };
+
   return (
-    <Editor
-      height={height}
-      width={width}
-      initialContent={map.get(activeTemplate)?.content || ""}
-      language="twig"
-      onChange={handleEditorOnChange}
-    />
+    <>
+      <div className="absolute top-0 right-0 z-10 px-6 py-2">
+        <Button.Icon
+          type="button"
+          Icon={CleanHand}
+          onClick={formatTemplate}
+          className="bg-transparent"
+        />
+      </div>
+
+      <Editor
+        ref={editorRef}
+        height={height}
+        width={width}
+        initialContent={templates[activeTemplate].content}
+        language="html"
+        setEditorContent={updateTemplate}
+      />
+    </>
   );
 };
 
--- src/features/EditorWindow/components/Editor.tsx
+++ src/features/EditorWindow/components/Editor.tsx
@@ -1,8 +1,9 @@
 import { default as Monaco, OnChange, OnMount } from "@monaco-editor/react";
-import { useState } from "react";
+import React, { forwardRef, useCallback } from "react";
+import debounce from "lodash.debounce";
 
 interface IEditorProps {
-  onChange?: (content: string) => void;
+  setEditorContent: (content: string) => void;
   initialContent: string;
   language: string;
   height?: number;
@@ -17,19 +18,65 @@ const options = {
   mouseWheelZoom: true,
 };
 
-const Editor: React.FC<IEditorProps> = (props) => {
-  const { initialContent, language, onChange, height, width } = props;
-  const [editorContent, setEditorContent] = useState(initialContent);
+const Editor = forwardRef<typeof Monaco, IEditorProps>(function Editor(
+  props,
+  ref
+) {
+  const { initialContent, language, setEditorContent, height, width } = props;
 
-  const handleOnMount: OnMount = (editor) => {
-    editor.getAction("editor.action.formatDocument").run();
+  const handleOnMount: OnMount = (editor, monaco) => {
+    if (ref) {
+      // @ts-ignore
+      ref.current = editor;
+    }
+
+    monaco.languages.registerCompletionItemProvider("html", {
+      triggerCharacters: [">"],
+      provideCompletionItems: (model: any, position: any) => {
+        const codePre: string = model.getValueInRange({
+          startLineNumber: position.lineNumber,
+          startColumn: 1,
+          endLineNumber: position.lineNumber,
+          endColumn: position.column,
+        });
+
+        const tag = codePre.match(/.*<(\w+)>$/)?.[1];
+
+        if (!tag) {
+          return;
+        }
+
+        const word = model.getWordUntilPosition(position);
+
+        return {
+          suggestions: [
+            {
+              label: `</${tag}>`,
+              kind: monaco.languages.CompletionItemKind.EnumMember,
+              insertText: `$1</${tag}>`,
+              insertTextRules:
+                monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+              range: {
+                startLineNumber: position.lineNumber,
+                endLineNumber: position.lineNumber,
+                startColumn: word.startColumn,
+                endColumn: word.endColumn,
+              },
+            },
+          ],
+        };
+      },
+    });
   };
 
   const handleOnChange: OnChange = (content) => {
-    if (!content) return;
-    setEditorContent(content);
-    return onChange && onChange(content);
+    setEditorContent(content || "");
   };
+
+  // const debouncedChangeHandler = useCallback(
+  //   debounce((content: string) => setEditorContent(content), 300),
+  //   [setEditorContent]
+  // );
 
   return (
     <div className="w-full h-full bg-[#1e1e1e] border-b border-neutral-700 overflow-hidden">
@@ -37,14 +84,14 @@ const Editor: React.FC<IEditorProps> = (props) => {
         theme="vs-dark"
         options={options}
         defaultLanguage={language}
-        defaultValue={editorContent}
-        onMount={handleOnMount}
+        defaultValue={initialContent}
         onChange={handleOnChange}
+        onMount={handleOnMount}
         height={height}
         width={width}
       />
     </div>
   );
-};
+});
 
 export default Editor;
