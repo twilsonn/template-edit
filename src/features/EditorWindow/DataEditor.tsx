33fddf860b6dc9bf176608c25b77c3467a520e57
Tyler Wilson <tylerwilson0729@outlook.com>
2023-03-25 13:15:41 +0000

    update: implemented data editor feature

--- src/features/EditorWindow/DataEditor.tsx
+++ src/features/EditorWindow/DataEditor.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import Editor from "./components/Editor";
+import { useAtom } from "jotai";
+import { templatesAtom } from "./state";
 
 interface IDataEditor {
   height: number;
@@ -7,11 +9,36 @@ interface IDataEditor {
 }
 
 const DataEditor: React.FC<IDataEditor> = ({ height, width }) => {
+  const [{ activeTemplate, templates }, setTemplates] = useAtom(templatesAtom);
+
+  const updateTemplate = (newData: any) => {
+    const currentTemplate = templates[activeTemplate];
+
+    if (currentTemplate) {
+      const newVersion = {
+        [activeTemplate]: {
+          ...currentTemplate,
+          lastUpdated: new Date(Date.now()).getTime(),
+          data: JSON.parse(newData),
+        },
+      };
+
+      setTemplates({
+        activeTemplate,
+        templates: {
+          ...templates,
+          ...newVersion,
+        },
+      });
+    }
+  };
+
   return (
     <Editor
+      setEditorContent={updateTemplate}
       height={height}
       width={width}
-      initialContent="test"
+      initialContent={JSON.stringify(templates[activeTemplate].data)}
       language="json"
     />
   );
