d9e28a86efb4eb4d494193c58c785ff19d8bf546
Tyler Wilson <tylerwilson0729@outlook.com>
2023-04-15 15:34:07 +0100

    update: added get time helper

--- src/features/EditorWindow/state/templateState.ts
+++ src/features/EditorWindow/state/templateState.ts
@@ -1,15 +1,16 @@
-import { MapOrEntries } from "usehooks-ts";
 import { atomWithLocalStorage } from "../../../utils/AtomWithLocalStorage";
 import { uniqueId } from "../../../utils/generateId";
 
-interface ITemplate {
+const getNewTime = () => new Date(Date.now()).getTime();
+
+export interface ITemplate {
   content: string;
   data: object;
   name: string;
   lastUpdated: number;
 }
 
-interface ITemplates {
+export interface ITemplates {
   activeTemplate: string;
   templates: {
     [key: string]: ITemplate;
@@ -25,7 +26,7 @@ const templatesAtom = atomWithLocalStorage<ITemplates>("editor.templates", {
       content:
         '<!DOCTYPE html>\r\n<html>\r\n\t<head>\r\n\t<title>My Webpage</title>\r\n\t</head>\r\n\t<body>\r\n\t<ul id="navigation">\r\n\t{% for item in navigation %}\r\n\t<li><a href="{{ item.href }}">{{ item.caption }}</a></li>\r\n\t{% endfor %}\r\n\t</ul>\r\n\r\n\t<h1>My Webpage</h1>\r\n\t{{ a_variable }}\r\n\t</body>\r\n</html>\r\n',
       name: "Default Template",
-      lastUpdated: new Date(Date.now()).getTime(),
+      lastUpdated: getNewTime(),
       data: {},
     },
   },
