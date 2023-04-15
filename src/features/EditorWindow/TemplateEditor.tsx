f6b58265c0bc3d79cf6e88d6f7781e7a978cff1b
Tyler Wilson <tylerwilson0729@outlook.com>
2023-04-15 15:36:40 +0100

    update: remove twig formatter

--- src/features/EditorWindow/TemplateEditor.tsx
+++ src/features/EditorWindow/TemplateEditor.tsx
@@ -1,41 +1,25 @@
 import { useEffect, useRef } from "react";
 import { useAtom } from "jotai";
 import Editor from "./components/Editor";
-import Button from "@/components/Button";
 import { templatesAtom } from "./state";
-import { CleanHand } from "@/assets/icons";
+import { ITemplate } from "./state/templateState";
+import { default as Monaco } from "@monaco-editor/react";
 
 interface ITemplateEditor {
   height: number;
   width: number;
 }
 
-const formatContent = (content: string) =>
-  fetch("/api/format", {
-    method: "POST", // *GET, POST, PUT, DELETE, etc.
-    mode: "cors",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      type: "twig",
-      content,
-    }),
-  })
-    .then((req) => req.json())
-    .then((data) => {
-      return data.content;
-    });
-
 const TemplateEditor: React.FC<ITemplateEditor> = ({ height, width }) => {
-  const editorRef = useRef(null);
+  const editorRef = useRef<typeof Monaco>(null);
   const [{ activeTemplate, templates }, setTemplates] = useAtom(templatesAtom);
 
-  useEffect(() => {
-    formatTemplate();
-  }, [activeTemplate]);
-
-  const updateTemplate = (newContent: string) => {
+  const updateTemplate = async (
+    templates: {
+      [key: string]: ITemplate;
+    },
+    activeTemplate: string
+  ) => {
     const currentTemplate = templates[activeTemplate];
 
     if (currentTemplate) {
@@ -43,7 +27,7 @@ const TemplateEditor: React.FC<ITemplateEditor> = ({ height, width }) => {
         [activeTemplate]: {
           ...currentTemplate,
           lastUpdated: new Date(Date.now()).getTime(),
-          content: newContent,
+          content: currentTemplate.content,
         },
       };
 
@@ -57,35 +41,32 @@ const TemplateEditor: React.FC<ITemplateEditor> = ({ height, width }) => {
     }
   };
 
-  const formatTemplate = () => {
-    const currentTemplate = templates[activeTemplate];
-    formatContent(currentTemplate.content).then((formattedContent) => {
-      updateTemplate(formattedContent);
-      // @ts-ignore
-      editorRef.current?.setValue(formattedContent);
-    });
+  useEffect(() => {
+    updateTemplate(templates, activeTemplate);
+  }, [activeTemplate]);
+
+  const handleEditorChanges = (newContent: string) => {
+    updateTemplate(
+      {
+        ...templates,
+        [activeTemplate]: {
+          ...templates[activeTemplate],
+          content: newContent,
+        },
+      },
+      activeTemplate
+    );
   };
 
   return (
-    <>
-      <div className="absolute top-0 right-0 z-10 px-6 py-2">
-        <Button.Icon
-          type="button"
-          Icon={CleanHand}
-          onClick={formatTemplate}
-          className="bg-transparent"
-        />
-      </div>
-
-      <Editor
-        ref={editorRef}
-        height={height}
-        width={width}
-        initialContent={templates[activeTemplate].content}
-        language="html"
-        setEditorContent={updateTemplate}
-      />
-    </>
+    <Editor
+      ref={editorRef}
+      height={height}
+      width={width}
+      initialContent={templates[activeTemplate].content}
+      language="html"
+      setEditorContent={handleEditorChanges}
+    />
   );
 };
 
