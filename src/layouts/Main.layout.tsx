83d7f38dda0057eab8ecfdf1c5b56bedeacb83ed
Tyler Wilson <tylerwilson0729@outlook.com>
2023-05-18 23:08:01 +0100

    add: added main layout file

---
+++ src/layouts/Main.layout.tsx
@@ -0,0 +1,33 @@
+import Navigation from "@/features/Navigation_";
+import dynamic from "next/dynamic";
+import Head from "next/head";
+
+const EditorController = dynamic(() => import("@/features/EditorController"), {
+  ssr: false,
+});
+
+const Main: React.FC<React.PropsWithChildren> = ({ children }) => {
+  return (
+    <>
+      <Head>
+        <meta name="description" content="TemplateEdit" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="w-screen h-screen flex flex-row">
+        <Navigation />
+
+        <div className="flex-grow">
+          <div className="px-3 w-full h-16 border-b flex items-center">
+            <EditorController />
+          </div>
+
+          <div className="flex-1 h-[calc(100%-4rem)]">{children}</div>
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default Main;
