55e9dee2e560ee3fd533e87821808bb9da849f56
Tyler Wilson <tylerwilson0729@outlook.com>
2023-05-18 23:08:30 +0100

    add: added editor page

---
+++ src/pages/editor.tsx
@@ -0,0 +1,40 @@
+import Head from "next/head";
+import dynamic from "next/dynamic";
+import { Suspense } from "react";
+import Main from "@/layouts/Main.layout";
+import { AppProps } from "next/app";
+import { useRouter } from "next/router";
+
+const Navigation = dynamic(() => import("@/features/Navigation"), {
+  ssr: false,
+});
+const EditorWindow = dynamic(() => import("@/features/EditorWindow"), {
+  ssr: false,
+});
+
+const Toaster = dynamic(() => import("@/components/Toaster"), {
+  ssr: false,
+});
+
+export default function Home({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  const id = router.query.id as string;
+
+  return (
+    <>
+      <Head>
+        <title>TemplateEdit - Editor ({id})</title>
+      </Head>
+
+      <Toaster position="bottom-right" reverseOrder={false} />
+
+      <Main>
+        <div className="h-screen w-screen flex flex-col">
+          <Suspense fallback="Loading...">
+            <EditorWindow id={id} />
+          </Suspense>
+        </div>
+      </Main>
+    </>
+  );
+}
