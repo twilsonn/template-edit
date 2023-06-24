6dcaddfd715a08a62e9faba9a73a9bfb64ce1212
Tyler Wilson <tylerwilson0729@outlook.com>
2023-06-24 15:24:39 +0100

    update: removed unused state

--- src/features/EditorWindow/state/index.ts
+++ src/features/EditorWindow/state/index.ts
@@ -1,2 +1,25 @@
-export { editorSizeAtom } from "./editorState";
-export { templatesAtom } from "./templateState";
+import { atomWithLocalStorage } from "@/utils/AtomWithLocalStorage";
+
+interface IEditorSize {
+  editor: {
+    width: number;
+    height: number;
+  };
+  window: {
+    width: number;
+    height: number;
+  };
+}
+
+const editorSizeAtom = atomWithLocalStorage<IEditorSize>("editor.size", {
+  editor: {
+    width: 0,
+    height: 0,
+  },
+  window: {
+    width: 0,
+    height: 0,
+  },
+});
+
+export { editorSizeAtom };
