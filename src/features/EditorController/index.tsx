2de5291ee929e86c64a5ac2585fa64f8c107a91f
Tyler Wilson <tylerwilson0729@outlook.com>
2023-06-24 15:25:26 +0100

    update: component now uses zustand state

--- src/features/EditorController/index.tsx
+++ src/features/EditorController/index.tsx
@@ -1,16 +1,25 @@
+import { MouseEvent } from "react";
 import { HomeIcon, XMarkIcon } from "@heroicons/react/24/solid";
-import { useAtom, useAtomValue } from "jotai";
 import Link from "next/link";
-import { controllerAtom } from "./controllerState";
-import { templatesAtom } from "../EditorWindow/state";
+import useTemplateStore from "@/store/templateStore";
+import { useRouter } from "next/router";
 
 const EditorController = () => {
-  const { templates } = useAtomValue(templatesAtom);
-  const [controller, setController] = useAtom(controllerAtom);
+  const { templates, open, closeTemplate, active } = useTemplateStore();
+  const router = useRouter();
 
-  const handleDeleteEditor = (key: string) => {
-    delete controller[key];
-    setController(controller);
+  const handleDeleteEditor = (
+    evt: MouseEvent<HTMLButtonElement, globalThis.MouseEvent>,
+    key: string
+  ) => {
+    evt.preventDefault();
+    evt.stopPropagation();
+
+    closeTemplate(key);
+
+    if (active === key) {
+      router.push(`/`);
+    }
   };
 
   return (
@@ -24,13 +33,13 @@ const EditorController = () => {
         </button>
       </Link>
 
-      {Object.keys(controller).map((key) => (
+      {open.map((key) => (
         <Link key={key} href={`/editor?id=${key}`}>
           <div className="h-10 rounded-md bg-gray-100 px-3 py-1.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-200 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-200 mr-2 flex items-center justify-between cursor-pointer select-none">
-            <p>{templates[key].name}</p>
+            <p>{templates[key]?.name}</p>
             <button
               className="ml-3 p-0.5 rounded-full group-hover:bg-gray-200 hover:bg-gray-300"
-              onClick={() => handleDeleteEditor(key)}
+              onClick={(evt) => handleDeleteEditor(evt, key)}
             >
               <XMarkIcon className="w-3 h-3" />
             </button>
