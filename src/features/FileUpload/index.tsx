7069300a58bf97326c52306f3e9e46bd12c95c65
Tyler Wilson <tylerwilson0729@outlook.com>
2024-03-20 21:28:32 +0000

    update: updated default template content

--- src/features/FileUpload/index.tsx
+++ src/features/FileUpload/index.tsx
@@ -1,13 +1,23 @@
-import React, { useEffect, useMemo, useRef, useState } from "react";
-import { decrypt, encrypt } from "crypto-js/aes";
-
+import React, { useRef } from "react";
+import { encrypt } from "crypto-js/aes";
 import { atomWithLocalStorage } from "@/utils/AtomWithLocalStorage";
 import { useAtom } from "jotai";
 import { encode } from "js-base64";
 import { DocumentArrowUpIcon } from "@heroicons/react/24/solid";
 import Dropzone, { Accept, DropEvent, FileRejection } from "react-dropzone";
 
-export const filesAtom = atomWithLocalStorage<IFiles>("files", {});
+export const filesAtom = atomWithLocalStorage<IFiles>("files", {
+  "aW5kZXguanM=": {
+    name: "index.js",
+    content:
+      "U2FsdGVkX18LlivuXuzAmXXK0cQkwJxmoXBW8Msc4v5MVL9bT68NM791v1wKdBN5HYLyscQCIo1jFmb/BVQTWACcp09W8SDoZdSuNDQsYmu9aL18Sw2SLuY1bINYuCJM0uZAydoAc9RYvPSqL1L9wC+JAsU2h16lJCrd4PEg34WxEHRuIXCiRY5Dz7NqbOdEdUK6wzRUwCnMi1rZVEZQY3x6MtdRZmB/Jqoxbl6qaRRKpykaaSfOJXgM8QxQbThP5iRmkutceZf0M0j/2AVvNN7O9tocTGppm4frKui0ITIzSIK4b3djNgWi372AmXQ26IpN9Hi0Jtq/0antk0SykoZBlmEY7nKxvxtUglrnHd6l/Lcwbku/Xo+uqI+MH8ij",
+    type: "script",
+    updatedAt: 1710968452951,
+    createdAt: 1687614675741,
+    html: '<script src="files/index.js"></script>',
+    size: 262,
+  },
+});
 
 const getTypeFromExtention = (name: string) => {
   const extension = name.split(".")[1];
@@ -25,11 +35,7 @@ const getTypeFromExtention = (name: string) => {
   return "unknown";
 };
 
-type DropHandler = <T extends File>(
-  acceptedFiles: T[],
-  fileRejections: FileRejection[],
-  event: DropEvent
-) => void;
+type DropHandler = <T extends File>(acceptedFiles: T[], fileRejections: FileRejection[], event: DropEvent) => void;
 
 const filesAccept: Accept = {
   ".js": [],
@@ -84,24 +90,17 @@ const FileUpload: React.FC = () => {
           <input {...getInputProps()} />
           <div
             className={`mt-2 flex justify-center rounded-lg border-2 border-dashed border-gray-900/25 px-6 py-10  ${
-              isDragActive
-                ? "border-blue-500 bg-blue-400/20"
-                : "border-blue-400 bg-blue-400/10"
+              isDragActive ? "border-blue-500 bg-blue-400/20" : "border-blue-400 bg-blue-400/10"
             }`}
           >
             <div className="text-center">
-              <DocumentArrowUpIcon
-                className="mx-auto h-8 w-8 text-blue-500"
-                aria-hidden="true"
-              />
+              <DocumentArrowUpIcon className="mx-auto h-8 w-8 text-blue-500" aria-hidden="true" />
               <div className="mt-4 flex text-lg font-semibold leading-6 text-gray-600">
                 <p className="pr-1">Drag and drop files, or</p>
                 <div
                   className={`relative cursor-pointer rounded-md text-blue-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-blue-600 focus-within:ring-offset-2 hover:text-blue-500`}
                 >
-                  <span onClick={() => fileInputRef.current?.click()}>
-                    Browse
-                  </span>
+                  <span onClick={() => fileInputRef.current?.click()}>Browse</span>
                 </div>
               </div>
               <p className="text-xs leading-5 text-gray-600">Up to 5MB</p>
